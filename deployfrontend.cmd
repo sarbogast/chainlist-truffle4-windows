4c4c2abd4611fb92d95390ca4f17008a1bbabcff
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2018-01-07 21:20:19 +0100

    Adding frontend files to Github Pages

--- deployfrontend.cmd
+++ deployfrontend.cmd
@@ -1,5 +1,5 @@
-robocopy -r src docs /mir /copy
-robocopy build\contracts docs /mir /copy
+robocopy -r src docs /mir
+robocopy build\contracts docs /mir
 git add .
 git commit -m "Adding frontend files to Github Pages"
 git push
